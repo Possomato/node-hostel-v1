98ea10041db9cfde7dd054956b0cab9da735d5ec
Possomato <gabrielb.possomato@gmail.com>
2025-06-14 10:47:09 -0300

    upd - hero and routes nav

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,5 +1,37 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express'
 
 export const home = (req: Request, res: Response) => {
-  res.render('pages/page')
+  res.render('pages/page', {
+    hero: {
+      title: 'Todos os hostels',
+      background: '',
+    },
+  })
+}
+
+export const sp = (req: Request, res: Response) => {
+  res.render('pages/page', {
+    hero: {
+      title: 'São Paulo',
+      background: '',
+    },
+  })
+}
+
+export const rj = (req: Request, res: Response) => {
+  res.render('pages/page', {
+    hero: {
+      title: 'Rio de Janeiro',
+      background: '',
+    },
+  })
+}
+
+export const sal = (req: Request, res: Response) => {
+  res.render('pages/page', {
+    hero: {
+      title: 'Salvador',
+      background: '',
+    },
+  })
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express'
 
-import * as PageController from '../controllers/PageController'
+import * as PageController from '../controllers/pageController'
 
 const router = Router()
 
 router.get('/', PageController.home)
-// router.get('/sp', PageController.sp)
-// router.get('/rj', PageController.rj)
-// router.get('/sal', PageController.sal)
+router.get('/sp', PageController.sp)
+router.get('/rj', PageController.rj)
+router.get('/sal', PageController.sal)
 
 // router.get('/search', PageController.search)
 
